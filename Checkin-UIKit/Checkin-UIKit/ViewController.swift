c99a94624e2fea3f66df10f49ce84c3a9b9edafe
LuccaPV <luccappadilha@gmail.com>
2026-04-23 14:08:45 -0300

    feat: adding the xCode default project to the repository

---
+++ Checkin-UIKit/Checkin-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Checkin-UIKit
+//
+//  Created by Lucca Pivoto on 23/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
